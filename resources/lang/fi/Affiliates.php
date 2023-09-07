d6b72d3f681d13217308b42e45df2af4d837c2d6
Tuomas Savela <tuomas@mobiilitaksi.com>
2023-09-07 18:55:38 +0300

    Update Affiliates.php

--- resources/lang/fi/Affiliates.php
+++ resources/lang/fi/Affiliates.php
@@ -5,8 +5,8 @@
 
 return array (
   //============================== New strings to translate ==============================//
-  'affiliate_name' => 'Affiliate Name',
-  'affiliate_tracking' => 'Affiliate Tracking',
+  'affiliate_name' => 'Affiliaatin nimi',
+  'affiliate_tracking' => 'Affiliaattiseuranta',
   'affiliate_tracking_text' => 'Keeping track of who is generating sales for your event is extremely easy. Simply create a referral link using the box below and share the link with your affiliates / event promoters.',
   'last_referral' => 'Last Referral',
   'no_affiliate_referrals_yet' => 'No Affiliate Referrals Yet',
